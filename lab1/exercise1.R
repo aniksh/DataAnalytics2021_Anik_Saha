021f1ea013c136d94d5bc89d4d412f7980eb9700
Anik Saha <aaniksaha@gmail.com>
2021-02-11 00:26:39 -0500

    lab1 EPI

---
+++ lab1/exercise1.R
@@ -0,0 +1,36 @@
+setwd("C:/Users/Anik/Desktop/data-analytics/labs/lab1")
+
+EPIData <- read.csv('2010EPI_data.csv', skip = 1) 
+# First row contains excel column names X1, X2 etc. So skipped.
+View(EPIData)
+
+attach(EPIData)
+fix(EPIData)
+
+EPI
+tf <- is.na(EPI)
+E <- EPI[!tf] # filters out NA values, new array
+
+summary(EPI)
+fivenum(EPI, na.rm = TRUE) # prints the min, q1, median, q3, max
+
+help(stem)
+stem(EPI)
+
+help(hist)
+hist(EPI)
+
+hist(EPI, seq(30., 95., 1.0), prob=TRUE)
+help(lines)
+lines(density(EPI, na.rm = TRUE, bw = 1.))
+help(rug)
+rug(EPI)
+
+help(plot)
+plot(ecdf(EPI), do.points = FALSE, verticals = TRUE)
+help(par)
+par(pty="s")
+qqnorm(EPI); qqline(EPI)
+
+x <- seq(30,95,1)
+qqplot(qt(ppoints(250), df = 5), x, xlab = "Q-Q plot for tdsn"); qqline(x)
---
+++ lab1/exercise2.R
@@ -0,0 +1,16 @@
+EPILand <- EPI[!Landlock]
+ELand <- EPILand[!is.na(EPILand)]
+hist(ELand)
+hist(ELand, seq(30., 95., 1.), prob = TRUE)
+lines(density(ELand, na.rm = TRUE, bw = 1.))
+rug(ELand)
+
+plot(ecdf(ELand), do.points = FALSE, verticals = TRUE)
+par(pty="s")
+qqnorm(EPI); qqline(EPI)
+
+EPI_South_Asia <- EPI[EPI_regions=="South Asia"]
+hist(EPI_South_Asia)
+hist(EPI_South_Asia, seq(40., 70., 1.), prob = TRUE)
+lines(density(EPI_South_Asia, na.rm = TRUE, bw = 1.))
+rug(EPI_South_Asia)
