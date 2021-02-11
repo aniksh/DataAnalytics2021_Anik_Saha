3d6feac4994b4bc73cd11eff0656d224cd1923c7
Anik Saha <aaniksaha@gmail.com>
2021-02-11 00:30:27 -0500

    Lab1 hist

--- lab1/exercise2.R
+++ lab1/exercise2.R
@@ -1,5 +1,6 @@
 EPILand <- EPI[!Landlock]
 ELand <- EPILand[!is.na(EPILand)]
+par(pty="m")
 hist(ELand)
 hist(ELand, seq(30., 95., 1.), prob = TRUE)
 lines(density(ELand, na.rm = TRUE, bw = 1.))
@@ -10,6 +11,7 @@ par(pty="s")
 qqnorm(EPI); qqline(EPI)
 
 EPI_South_Asia <- EPI[EPI_regions=="South Asia"]
+par(pty="m")
 hist(EPI_South_Asia)
 hist(EPI_South_Asia, seq(40., 70., 1.), prob = TRUE)
 lines(density(EPI_South_Asia, na.rm = TRUE, bw = 1.))
